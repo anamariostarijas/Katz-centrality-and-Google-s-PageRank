753035efa72f9dac7a9e00e64f910b278237adb2
Tina Razic <tina.razic@gmail.com>
2019-01-02 12:04:41 +0100

    Katz dokumentacija

---
+++ Testni algoritmi/katz.py
@@ -0,0 +1,42 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+import numpy as np
+from timeit import default_timer as timer
+import math
+import scipy.stats as stats
+
+T = nx.DiGraph()   # or DiGraph, etc
+T.add_edges_from(((1, 2), (2, 3), (2, 4), (2, 5), (5, 2)))
+nx.draw(T, with_labels=True)
+plt.show()
+
+
+def rank(array):
+    '''Returns vector with ranked values of array,
+    where 0 is the minimum value of array'''
+    temp = array[:, 0].argsort(axis=0)
+    ranks = temp[:, 0].argsort(axis=0)
+    return ranks
+
+
+def katz(graph, max_num_of_steps, tolerance, alpha, beta):
+    '''Computes the Katz centrality for the nodes of the graph G.'''
+    start = timer()
+    A = nx.adjacency_matrix(graph)
+    # first guess for vector r is first column of A
+    r = A[:, 0]
+    diff = 1000
+    k = 0
+    vector = np.ones((A.shape[1], 1))
+    while diff > tolerance and k < max_num_of_steps:
+        # inner product of matrix A and vector r
+        r, q = alpha*A.dot(r) + beta * vector, r
+        diff = np.linalg.norm(q-r, ord=1)
+        k += 1
+    print('process finished after {} iterations'.format(k))
+    end = timer()
+    # timing of the process
+    print('time consumption: {} seconds'.format(end-start))
+    return r
+
+test = katz(T, 100, 0.1, 0.2, 1)
